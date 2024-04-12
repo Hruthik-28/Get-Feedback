6736c1889293a2fe88344ddb4f4e039222a16917
HRUTHIK <hruthik9980819630@gmail.com>
2024-04-12 13:56:46 +0530

    nextjs project setup- add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,74 @@
+import { Schema, Document, model, models, Model } from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        requireed: [true, "content is required"],
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now(),
+    },
+});
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    acceptMessages: boolean;
+    isVerified: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        match: [
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+            "email is not valid",
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "verifyCode is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "verifyCodeExpiry is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    acceptMessages: {
+        type: Boolean,
+        default: true,
+    },
+    messages: {
+        type: [MessageSchema],
+    },
+});
+
+const User = (models.users as Model<User>) || model<User>("User", UserSchema);
+
+export default User;
