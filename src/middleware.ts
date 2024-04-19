d91359963fcc2ab795ac5fdb4f1f8eba4bf839f8
HRUTHIK <hruthik9980819630@gmail.com>
2024-04-19 12:41:52 +0530

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === "/sign-in" || path === "/sign-up" || path === "/verify";
+
+    if (token && isPublicPath) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    if (!token && !isPublicPath) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+}
+
+export const config = {
+    matcher: ["/", "/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
+};
