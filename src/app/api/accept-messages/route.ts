69538bb06094e928a325242451694b83ea3661aa
HRUTHIK <hruthik9980819630@gmail.com>
2024-04-21 11:47:05 +0530

    add route acceptMessages
    - POST acceptMessages to toggle acceptMessage
    - GET acceptMessages to get accept messages status

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,109 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        const user: User = session?.user as User;
+
+        if (!session || !session.user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Unauthorized request. Please sign-in",
+                },
+                { status: 401 }
+            );
+        }
+
+        const userId = user?._id;
+        const { acceptMessages } = await request.json();
+
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            {
+                acceptMessages,
+            },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Failed to toggle accept messages",
+                },
+                { status: 401 }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Message acceptance status toggled successfully",
+                user,
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error(error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to toogle accept messages status",
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function GET(request: Request) {
+    await dbConnect();
+    try {
+        const session = await getServerSession(authOptions);
+        const user: User = session?.user as User;
+
+        if (!session || !session.user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Unauthorized request. Please sign-in",
+                },
+                { status: 401 }
+            );
+        }
+
+        const foundUser = await UserModel.findById(user._id);
+
+        if (!foundUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                { status: 404 }
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                acceptMessages: foundUser.acceptMessages,
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error(error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to get accept messages status",
+            },
+            { status: 500 }
+        );
+    }
+}
