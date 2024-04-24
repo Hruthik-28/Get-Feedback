cb8e54f8b8abcb32069f6a2e02a32095e77b48e0
HRUTHIK <hruthik9980819630@gmail.com>
2024-04-24 08:21:21 +0530

    changes in suggest-messages(temporary)

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -11,18 +11,28 @@ export async function POST(req: Request) {
     try {
         const prompt =
             "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be? || What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+        let response;
+        try {
+            response = await openai.completions.create({
+                model: "gpt-3.5-turbo-instruct",
+                max_tokens: 400,
+                stream: false,
+                prompt,
+            });
+        } catch (error) {
+            console.error(error);
+            return Response.json({ success: false, error }, { status: 200 });
+        }
 
-        const response = await openai.completions.create({
-            model: "gpt-3.5-turbo-instruct",
-            max_tokens: 400,
-            stream: true,
-            prompt,
-        });
+        // // Convert the response into a friendly text-stream
+        // const stream = OpenAIStream(response);
+        // // Respond with the stream
+        // const message = new StreamingTextResponse(stream);
 
-        // Convert the response into a friendly text-stream
-        const stream = OpenAIStream(response);
-        // Respond with the stream
-        const message = new StreamingTextResponse(stream);
+        return Response.json(
+            { success: true, message: response },
+            { status: 200 }
+        );
     } catch (error: any) {
         if (error instanceof OpenAI.APIError) {
             const { name, status, headers, message } = error;
